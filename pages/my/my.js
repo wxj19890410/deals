21e264edac1f681ea0a0c8661f4a1a68b9b07141
wxj <1213510363@qq.com>
2018-09-17 17:27:53 +0800

    Initial commit

---
+++ pages/my/my.js
@@ -0,0 +1,33 @@
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    userListInfo: [{
+      icon: '../../images/iconfont-icontuan.png',
+      text: '好友',
+      isunread: true,
+      unreadNum: 1
+    },{
+      icon: '../../images/iconfont-dingdan.png',
+      text: '完成的计划',
+      isunread: true,
+      unreadNum: 2
+    }, {
+      icon: '../../images/iconfont-card.png',
+      text: '放弃的计划',
+      isunread: false,
+      unreadNum: 2
+    }]
+  },
+
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  }
+})

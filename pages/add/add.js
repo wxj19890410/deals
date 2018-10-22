21c980e8637303dd95b078c600da7c0905f0e572
wxj19890410 <1213510363@qq.com>
2018-10-22 23:44:20 +0800

    --wxj

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,101 +1,4 @@
 Page({
-<<<<<<< HEAD
-  data: {
-    array: ['美国', '中国', '巴西', '日本'],
-    objectArray: [
-      {
-        id: 0,
-        name: '美国'
-      },
-      {
-        id: 1,
-        name: '中国'
-      },
-      {
-        id: 2,
-        name: '巴西'
-      },
-      {
-        id: 3,
-        name: '日本'
-      }
-    ],
-    index: 0,
-    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
-    objectMultiArray: [
-      [
-        {
-          id: 0,
-          name: '无脊柱动物'
-        },
-        {
-          id: 1,
-          name: '脊柱动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '扁性动物'
-        },
-        {
-          id: 1,
-          name: '线形动物'
-        },
-        {
-          id: 2,
-          name: '环节动物'
-        },
-        {
-          id: 3,
-          name: '软体动物'
-        },
-        {
-          id: 3,
-          name: '节肢动物'
-        }
-      ], [
-        {
-          id: 0,
-          name: '猪肉绦虫'
-        },
-        {
-          id: 1,
-          name: '吸血虫'
-        }
-      ]
-    ],
-    multiIndex: [0, 0, 0],
-    date: '2016-09-01',
-    time: '12:01'
-  },
-  bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      multiIndex: e.detail.value
-    })
-  },
-  formSubmit: function (e) {
-    wx.chooseImage({
-      success(res) {
-        const tempFilePaths = res.tempFilePaths
-        console.log(tempFilePaths[0])
-        wx.uploadFile({
-          url: 'http://127.0.0.1/user/upload', //仅为示例，非真实的接口地址
-          filePath: tempFilePaths[0],
-          name: 'file',
-          formData: {
-            'user': 'test'
-          },
-          success(res) {
-            const data = res.data
-            //do something
-          }
-        })
-      }
-    })
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-  }
-=======
     data: {
         array: ['美国', '中国', '巴西', '日本'],
         objectArray: [
@@ -171,7 +74,19 @@ Page({
         })
     },
     formSubmit: function (e) {
+        console.log(222)
+        wx.request({
+          url: 'http://127.0.0.1:8083/user/loadInfo', //仅为示例，并非真实的接口地址
+          data: {
+            id: 1
+          },
+          header: {
+            'content-type': 'application/json' // 默认值
+          },
+          success(res) {
+            console.log(res.data)
+          }
+        })
         console.log('form发生了submit事件，携带数据为：', e.detail.value)
     }
->>>>>>> 0155ed9bdb3c333983f1451007171298e13c445b
 })
